0bc8999c47f4f4432a1d24ba1c4d7b8bd6d78576
SamariumCode <cactuspydon@gmail.com>
2024-06-14 19:45:01 +0330

    Custom admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Post)
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'slug', 'updated')
+    search_fields = ('slug',)
+    list_filter = ('updated',)
+    prepopulated_fields = {'slug': ('body',)}
+    raw_id_fields = ('user',)
