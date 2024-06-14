9ba781435f897906c6306abd3b4bbb8b83beb61e
SamariumCode <cactuspydon@gmail.com>
2024-06-14 10:28:14 +0330

    add login user in CBV View

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
 from django.views import View
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
 from . forms import UserRegisterForm
 
 
 class RegisterView(View):
+
+    form_class = UserRegisterForm
+    template_name = 'accounts/register.html'
+
     def get(self, request):
-        form = UserRegisterForm()
-        return render(request, 'accounts/register.html', {'form': form})
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(
+                username=cd['username'], email=cd['email'], password=cd['password'])
+            messages.success(
+                request, 'شما با موفقیت ثبت نام شدید', extra_tags='success')
+            return redirect('home:home')
+        return render(request, self.template_name, {'form': form})
