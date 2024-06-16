16e59d7bb09d46e2dcf684d7888e0da87321a290
SamariumCode <cactuspydon@gmail.com>
2024-06-16 18:21:20 +0330

    change delete view , update form

--- home/urls.py
+++ home/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('post/<int:pk>/<slug:slug>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/delete/<int:pk>/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/update/<int:pk>/', views.PostUpdateView.as_view(), name='post-update'),
 
 ]
--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from .models import Post
+from .forms import PostUpdateForm
 
 
 class HomeView(View):
@@ -22,18 +23,45 @@ class PostDetailView(View):
 
 class PostDeleteView(LoginRequiredMixin, View):
 
+    def dispatch(self, request, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+
+        if not post.user.id == request.user.id:
+            messages.error(request, 'شما اجازه حذف این پست رو ندارید', extra_tags='danger')
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
-        if post.user.id == request.user.id:
-            return render(request, 'home/post_confirm_delete.html', {'post': post})
-        else:
-            return redirect('home:home')
+        return render(request, 'home/post_confirm_delete.html', {'post': post})
 
     def post(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
-        if post.user.id == request.user.id:
-            post.delete()
-            messages.success(request, 'پست شما با موفقیت حذف شد', extra_tags='success')
-        else:
-            messages.error(request, 'شما اجازه ندارید این پست رو حذف کنید', extra_tags='danger')
+        post.delete()
+        messages.success(request, 'پست شما با موفقیت حذف شد', extra_tags='success')
         return redirect('home:home')
+
+
+class PostUpdateView(LoginRequiredMixin, View):
+    form_class = PostUpdateForm
+
+    def dispatch(self, request, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        if not post.user.id == request.user.id:
+            messages.error(request, 'شما اجازه بروزرسانی پست رو ندارید', extra_tags='danger')
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        form = self.form_class(instance=post)
+        return render(request, 'home/update.html', {'post': post, 'form': form})
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        form = self.form_class(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'اطلاعات پست با موفقیت تغییر کرد', extra_tags='success')
+            return redirect('home:home')
+        return render(request, 'home/update.html', {'form': form})
