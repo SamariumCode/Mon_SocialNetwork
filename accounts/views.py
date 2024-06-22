faa858ead846bc2e748aa1a3db8fba3e326bf2b4
SamariumCode <cactuspydon@gmail.com>
2024-06-22 11:40:54 +0330

    add next url for login

--- accounts/views.py
+++ accounts/views.py
@@ -45,6 +45,10 @@ class UserLoginView(View):
     form_class = UserLoginForm
     template_name = 'accounts/login.html'
 
+    def setup(self, request, *args, **kwargs):
+        self.next = request.GET.get('next')
+        return super().setup(request, *args, **kwargs)
+
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('home:home')
@@ -63,6 +67,9 @@ class UserLoginView(View):
             if user is not None:
                 login(request, user)
                 messages.success(request, 'کاربر گرامی شما با موفقیت وارد شدید', extra_tags='success')
+
+                if self.next:
+                    return redirect(self.next)
                 return redirect('home:home')
             messages.error(request, 'ایمیل یا رمز عبور شما درست وارد نشده است', extra_tags='warning')
         return render(request, self.template_name, {'form': form})
