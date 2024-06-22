00508e25570085b62013e76adbbb2531618edcb0
SamariumCode <cactuspydon@gmail.com>
2024-06-22 14:10:12 +0330

    add setup for detail view, method decorator for post

--- home/forms.py
+++ home/forms.py
@@ -11,4 +11,7 @@ class PostCreateUpdateForm(forms.ModelForm):
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['body', ]
+        fields = ['body']
+        widgets = {
+            'body': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'لطفاً نظرتان را در اینجا بنویسید'}),
+        }
--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
+from django.utils.decorators import method_decorator
 from django.utils.text import slugify
 from django.views import View
 
@@ -17,25 +19,30 @@ class HomeView(View):
 
 
 class PostDetailView(View):
-    def get(self, request, pk, slug):
-        post = get_object_or_404(Post, pk=pk, slug=slug)
-        comments = post.pcomments.filter(is_reply=False)
-        form = CommentForm()
-        return render(request, 'home/detail.html', {'post': post, 'comments': comments, 'form': form})
+    form_class = CommentForm
 
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = get_object_or_404(Post, pk=kwargs['pk'], slug=kwargs['slug'])
+        return super().setup(request, *args, **kwargs)
+
+    def get(self, request, pk, slug):
+        comments = self.post_instance.pcomments.filter(is_reply=False)
+        return render(request, 'home/detail.html',
+                      {'post': self.post_instance, 'comments': comments, 'form': self.form_class})
+
+    @method_decorator(login_required)
     def post(self, request, pk, slug):
-        post = get_object_or_404(Post, pk=pk, slug=slug)
-        form = CommentForm(request.POST)
+        form = self.form_class(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.post = post
+            comment.post = self.post_instance
             comment.user = request.user
             comment.save()
             messages.success(request, 'نظر شما با موفقیت ذخیره شد', extra_tags='success')
-            return redirect('home:post-detail', pk=post.pk, slug=slug)
+            return redirect('home:post-detail', pk=self.post_instance.pk, slug=self.post_instance.slug)
         else:
-            comments = post.pcomments.filter(is_reply=False)
-        return render(request, 'home/detail.html', {'post': post, 'form': form, 'comments': comments})
+            comments = self.post_instance.pcomments.filter(is_reply=False)
+        return render(request, 'home/detail.html', {'post': self.post_instance, 'form': form, 'comments': comments})
 
 
 class PostDeleteView(LoginRequiredMixin, View):
