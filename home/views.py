56efca7f7606bac8e037265802f0a9d9469c8a8c
SamariumCode <cactuspydon@gmail.com>
2024-06-24 21:20:55 +0330

    slugfy persian

--- home/views.py
+++ home/views.py
@@ -5,11 +5,28 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils.decorators import method_decorator
-from django.utils.text import slugify
+# from django.utils.text import slugify
+from slugify import slugify
 from django.views import View
 
 from .models import Post, Comment
 from .forms import PostCreateUpdateForm, CommentForm
+
+
+def persian_to_slug(text):
+    persian_to_latin_map = {
+        'آ': 'a', 'ا': 'a', 'ب': 'b', 'پ': 'p', 'ت': 't', 'ث': 's', 'ج': 'j',
+        'چ': 'ch', 'ح': 'h', 'خ': 'kh', 'د': 'd', 'ذ': 'z', 'ر': 'r', 'ز': 'z',
+        'ژ': 'zh', 'س': 's', 'ش': 'sh', 'ص': 's', 'ض': 'z', 'ط': 't', 'ظ': 'z',
+        'ع': 'e', 'غ': 'gh', 'ف': 'f', 'ق': 'gh', 'ک': 'k', 'گ': 'g', 'ل': 'l',
+        'م': 'm', 'ن': 'n', 'و': 'v', 'ه': 'h', 'ی': 'y', 'ئ': 'y', ' ': '-',
+        '‌': '-'
+    }
+
+    slug = ''.join(persian_to_latin_map.get(char, '') for char in text)
+    slug = ''.join(char for char in slug if char.isalnum() or char == '-')
+    slug = slug.strip('-')
+    return slug
 
 
 class HomeView(View):
@@ -22,7 +39,8 @@ class PostDetailView(View):
     form_class = CommentForm
 
     def setup(self, request, *args, **kwargs):
-        self.post_instance = get_object_or_404(Post, pk=kwargs['pk'], slug=kwargs['slug'])
+        self.post_instance = get_object_or_404(
+            Post, pk=kwargs['pk'], slug=kwargs['slug'])
         return super().setup(request, *args, **kwargs)
 
     def get(self, request, pk, slug):
@@ -38,10 +56,13 @@ class PostDetailView(View):
             comment.post = self.post_instance
             comment.user = request.user
             comment.save()
-            messages.success(request, 'نظر شما با موفقیت ذخیره شد', extra_tags='success')
+            messages.success(
+                request, 'نظر شما با موفقیت ذخیره شد', extra_tags='success')
             return redirect('home:post-detail', pk=self.post_instance.pk, slug=self.post_instance.slug)
         else:
             comments = self.post_instance.pcomments.filter(is_reply=False)
+            messages.warning(
+                request, '.لطفا درست و حسابی فرم رو پر کن', extra_tags='warning')
         return render(request, 'home/detail.html', {'post': self.post_instance, 'form': form, 'comments': comments})
 
 
@@ -51,7 +72,8 @@ class PostDeleteView(LoginRequiredMixin, View):
         post = get_object_or_404(Post, pk=kwargs['pk'])
 
         if not post.user.id == request.user.id:
-            messages.error(request, 'شما اجازه حذف این پست رو ندارید', extra_tags='danger')
+            messages.error(
+                request, 'شما اجازه حذف این پست رو ندارید', extra_tags='danger')
             return redirect('home:home')
         return super().dispatch(request, *args, **kwargs)
 
@@ -62,7 +84,8 @@ class PostDeleteView(LoginRequiredMixin, View):
     def post(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
         post.delete()
-        messages.success(request, 'پست شما با موفقیت حذف شد', extra_tags='success')
+        messages.success(request, 'پست شما با موفقیت حذف شد',
+                         extra_tags='success')
         return redirect('home:home')
 
 
@@ -76,7 +99,8 @@ class PostUpdateView(LoginRequiredMixin, View):
     def dispatch(self, request, *args, **kwargs):
         post = self.post_instance
         if not post.user.id == request.user.id:
-            messages.error(request, 'شما اجازه بروزرسانی پست رو ندارید', extra_tags='danger')
+            messages.error(
+                request, 'شما اجازه بروزرسانی پست رو ندارید', extra_tags='danger')
             return redirect('home:home')
         return super().dispatch(request, *args, **kwargs)
 
@@ -90,10 +114,11 @@ class PostUpdateView(LoginRequiredMixin, View):
         form = self.form_class(request.POST, instance=post)
         if form.is_valid():
             new_post = form.save(commit=False)
-            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.slug = persian_to_slug(form.cleaned_data['body'][:30])
             new_post.save()
 
-            messages.success(request, 'اطلاعات پست با موفقیت تغییر کرد', extra_tags='success')
+            messages.success(
+                request, 'اطلاعات پست با موفقیت تغییر کرد', extra_tags='success')
             return redirect('home:post-detail', post.id, post.slug)
         return render(request, 'home/update.html', {'form': form})
 
@@ -109,10 +134,11 @@ class PostCreateView(LoginRequiredMixin, View):
         form = self.form_class(request.POST)
         if form.is_valid():
             new_post = form.save(commit=False)
-            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.slug = persian_to_slug(form.cleaned_data['body'][:30])
             new_post.user = request.user
             new_post.save()
-            messages.success(request, 'پست شما با موفقیت ذخیره شد', extra_tags='success')
+            messages.success(
+                request, 'پست شما با موفقیت ذخیره شد', extra_tags='success')
             return redirect('home:post-detail', new_post.id, new_post.slug)
 
         return render(request, 'home/create.html', {'form': form})
